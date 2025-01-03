f90f0246cde6eac61e1e33364c19d0d0a8508842
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2025-01-03 20:48:10 +0530

    faq page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Recruiter from "./Pages/Recruiter";
 import Sdashboard from "./Pages/Sdashboard";
 import Rdashboard from "./Pages/Rdashboard";
 import Pdashboard from "./Pages/Pdashboard";
-
+import Faqs from "./Pages/Faqs";
 
 const App = () => {
 
@@ -28,6 +28,7 @@ const App = () => {
         <Route path="/login" element={<Login/>} />
         <Route path="/placements" element={<Placement/>} />
         <Route path="/recruiter" element={<Recruiter/>} />
+        <Route path="/faq" element={<Faqs />} />
         <Route path="/sdashboard/*" element={authUser ? <Sdashboard /> : <Navigate to="/"/>}/>         
         <Route path="/rdashboard" element={authUser ? <Rdashboard /> : <Navigate to="/"/>} />
         <Route path="/pdashboard" element={authUser ? <Pdashboard /> : <Navigate to="/"/>} />
