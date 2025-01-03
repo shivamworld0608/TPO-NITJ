9f40fdd60cd63175a75566bb5dcfb5a2d59284aa
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-01-03 20:53:28 +0530

    added whyrecruit page

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -70,7 +70,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="/"
+                href="/whyrecruit"
                 className="text-custom-blue font-semibold hover:text-blue-800"
               >
                 Why Recruit?
