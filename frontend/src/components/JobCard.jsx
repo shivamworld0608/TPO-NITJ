22ae3ef8060487da5fbf7e5d2146af8e0882e795
Abhinav Sharma <abhinavs.cs.22@nitj.ac.in>
2025-01-04 19:45:33 +0530

    Job profile

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const JobCard = ({ job_id, jobtype, jobtitle, company, deadline, onShowDetails }) => (
-  <div className="w-full max-w-lg mx-auto border bg-white rounded-lg shadow-lg p-6 transition-all transform duration-300 hover:shadow-2xl hover:scale-105">
+  <div className="w-full max-w-lg mx-auto border border-custom-blue bg-white rounded-lg shadow-lg p-6 transition-all transform duration-300 hover:shadow-2xl hover:scale-105">
       <h2 className="text-2xl font-semibold text-gray-800">{company}</h2>
       <p className="text-lg text-gray-600 mt-2">{jobtitle}</p>
       <div className="mt-4 text-sm text-gray-500">
