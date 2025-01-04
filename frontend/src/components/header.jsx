8f34b2445fddcdae4b6ca16db6092a3ae948c611
premkumar-004 <premkumarkardale@gmail.com>
2025-01-04 14:05:04 +0530

    adjusted curve image according to the small screen

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -281,7 +281,7 @@ const Header = () => {
         </div>
         {/* <!-- INSTITUTE LOGO & NAME ends -->
         <!-- Logo CONTAINER starts --> */}
-        <div className="absolute  sm:block left-[50px] z-40 mx-auto -translate-x-1/2 -translate-y-1/3 sm:translate-y-0 scale-75 sm:scale-100 sm:left-1/2 " style={{ width: "230px" }}>
+        <div className="absolute hidden sm:block left-[50px] z-40 mx-auto -translate-x-1/2 -translate-y-1/3 sm:translate-y-0 scale-75 sm:scale-100 sm:left-1/2 " style={{ width: "230px" }}>
           <img src="Rectangle.png" alt="" />
         </div>
         <a href="/index.html">
