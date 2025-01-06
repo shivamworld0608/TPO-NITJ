695743845b7227e72c36597940d28f66f5a5bfdb
Raghav Kukkar <kukkarraghav02@gmail.com>
2025-01-06 20:10:14 +0530

    Insights

---
+++ frontend/src/Pages/Insight.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Insights from "../components/Insights";
+
+function Insight() {
+  return (
+    <>
+      <Header />
+      <Insights />
+      <Footer/>
+    </>
+  )
+}
+
+export default Insight;
+
