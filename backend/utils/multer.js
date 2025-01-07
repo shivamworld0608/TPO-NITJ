515fe18da2618728842de79456bca4602dc8e63f
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2025-01-08 00:26:08 +0530

    Update multer.js

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -1,23 +1,12 @@
 import multer from 'multer';
-import path from 'path';
-import url from 'url';
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const baseDir = process.env.NODE_ENV === "production" ? '/tmp' : __dirname;
-const uploadsDir = path.join(baseDir, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadsDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  }
-});
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    }
+  });
   
   const upload = multer({ storage: storage });
 
