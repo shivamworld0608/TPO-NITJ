b8faa3a772c0a70b402ded564d5f9cf75e03d5ae
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2025-01-08 16:59:55 +0530

    fixed

--- frontend/src/Pages/Sdashboard.jsx
+++ frontend/src/Pages/Sdashboard.jsx
@@ -5,9 +5,17 @@ import Sdashboards from "../components/sdashboard";
 const SDashboard = () => {
   return (
     <>
-      <Header />
-      <div className="mt-36"><Sdashboards /></div>
-      <Footer/>
+      {/* <Header /> */}
+      <div className="mt-3"><Sdashboards /></div>
+      {/* <Footer/> */}
+      <div className="border-t  border-zinc-400 flex flex-col bg-slate-800 px-24 p-[20px] text-center text-white lg:flex-row justify-between ">
+        <div className="pb-[10px] text-[12px] lg:pb-0 lg:text-[15px]">
+          © Copyright 2022, All Rights Reserved NIT Jalandhar
+        </div>
+        <div className="pb-[10px] text-[12px] lg:pb-0 lg:text-[15px]">
+          Developed By <a href="/team" className="text-yellow-300 hover:text-yellow-300">Placement Portal Dev Team</a>
+        </div>
+      </div>
     </>
   );
 };
