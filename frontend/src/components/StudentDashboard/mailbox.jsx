c9a98814d8cd715c528c3c7bf18731d7938b418d
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2025-01-09 23:33:42 +0530

    changes mailbox and shared experiences

--- frontend/src/components/StudentDashboard/mailbox.jsx
+++ frontend/src/components/StudentDashboard/mailbox.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaSearch, FaEnvelope, FaTimes, FaArrowLeft } from "react-icons/fa";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 const sampleMessages = [
   {
@@ -9,7 +9,8 @@ const sampleMessages = [
     sender: "john.doe@example.com",
     subject: "Job Application Update",
     preview: "Your application has been reviewed and...",
-    fullMessage: "Your application has been reviewed and we would like to inform you that you have been shortlisted for the next round of interviews.",
+    fullMessage:
+      "Your application has been reviewed and we would like to inform you that you have been shortlisted for the next round of interviews.",
     timestamp: new Date().getTime() - 3600000, // 1 hour ago
     read: false,
     category: "Inbox",
@@ -19,7 +20,8 @@ const sampleMessages = [
     sender: "jane.smith@example.com",
     subject: "Interview Invitation",
     preview: "We would like to invite you for an interview...",
-    fullMessage: "We would like to invite you for an interview on Monday at 10 AM. Please confirm your availability.",
+    fullMessage:
+      "We would like to invite you for an interview on Monday at 10 AM. Please confirm your availability.",
     timestamp: new Date().getTime() - 7200000, // 2 hours ago
     read: true,
     category: "Sent",
@@ -29,7 +31,8 @@ const sampleMessages = [
     sender: "hr@company.com",
     subject: "Offer Letter",
     preview: "Congratulations! We are pleased to offer you the position...",
-    fullMessage: "Congratulations! We are pleased to offer you the position of Software Engineer. Please find the attached offer letter for your review.",
+    fullMessage:
+      "Congratulations! We are pleased to offer you the position of Software Engineer. Please find the attached offer letter for your review.",
     timestamp: new Date().getTime() - 10800000, // 3 hours ago
     read: false,
     category: "Pending",
@@ -54,7 +57,9 @@ const MessageItem = ({ message, onMarkAsRead, onDelete, onSelect }) => {
   return (
     <div
       className={`flex items-center p-4 mb-4 rounded-md border transition-all duration-300 ${
-        read ? "bg-gray-100 border-gray-300" : "bg-white border-custom-blue  hover:border-blue-500"
+        read
+          ? "bg-gray-100 border-gray-300"
+          : "bg-white border-custom-blue  hover:border-blue-500"
       }`}
       onClick={() => onSelect(message)}
     >
@@ -123,7 +128,8 @@ const MailboxComponent = () => {
     const matchesSearch =
       message.sender.toLowerCase().includes(searchTerm.toLowerCase()) ||
       message.subject.toLowerCase().includes(searchTerm.toLowerCase());
-    const matchesFilter = selectedFilter === "All" || message.category === selectedFilter;
+    const matchesFilter =
+      selectedFilter === "All" || message.category === selectedFilter;
     return matchesSearch && matchesFilter;
   });
 
@@ -170,96 +176,95 @@ const MailboxComponent = () => {
 
   return (
     <div className="p-6 w-full flex flex-col space-y-4">
-      <div className="bg-white p-6 ">
-      <h1 className="font-bold text-2xl sm:text-3xl lg:text-4xl text-center tracking-wide mb-8">
+     {/*  <div className="bg-white p-6 "> */}
+        {/* <h1 className="font-bold text-2xl sm:text-3xl lg:text-4xl text-center tracking-wide mb-8">
         Mail
         <span className="bg-custom-blue text-transparent bg-clip-text">
           Box
         </span>
-      </h1>
+      </h1> */}
         <div className="mb-4 flex items-center gap-2">
-  <FaSearch className="text-custom-blue" />
-  <input
-    type="text"
-    className="p-2 border rounded-md w-3/6" 
-    placeholder="Search Messages"
-    value={searchTerm}
-    onChange={handleSearchChange}
-  />
- 
-  {["All", "Inbox", "Sent", "Pending", "Draft"].map((filter) => (
-    <button
-      key={filter}
-      onClick={() => handleFilterChange(filter)}
-      className={`flex-1 p-2 rounded-3xl ${
-        selectedFilter === filter
-          ? "bg-custom-blue text-white"
-          : "bg-gray-200 text-gray-700"
-      }`}
-    >
-      {filter}
-    </button>
-  ))}
-   <button
-    onClick={handleComposeMessage}
-    className="bg-custom-blue text-white px-4 py-2 rounded-3xl hover:bg-blue-600"
-  >
-   <FontAwesomeIcon icon={faPenToSquare} className="p-0.5" />
-  </button>
-</div>
-{isComposing && (
-  <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
-    <div className="p-6 bg-white shadow-md rounded-md w-1/3">
-    <div className="flex justify-between">
+          <FaSearch className="text-custom-blue" />
+          <input
+            type="text"
+            className="p-2 border border-gray-300 rounded-2xl w-3/6"
+            placeholder="Search Messages"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
 
-      <h3 className="text-xl font-semibold mb-4">Compose Message</h3>
-      <button
-        onClick={() => setIsComposing(false)}
-        className="text-gray-500 mb-4"
-        >
-        <FaTimes className="text-custom-blue "/>
-      </button>
+          {["All", "Inbox", "Sent", "Pending", "Draft"].map((filter) => (
+            <button
+              key={filter}
+              onClick={() => handleFilterChange(filter)}
+              className={`flex-1 p-2 rounded-3xl ${
+                selectedFilter === filter
+                  ? "bg-custom-blue text-white"
+                  : "bg-gray-200 text-gray-700"
+              }`}
+            >
+              {filter}
+            </button>
+          ))}
+          <button
+            onClick={handleComposeMessage}
+            className="bg-custom-blue text-white px-4 py-2 rounded-3xl hover:bg-blue-600"
+          >
+            <FontAwesomeIcon icon={faPenToSquare} className="p-0.5" />
+          </button>
         </div>
-      <input
-        type="text"
-        name="sender"
-        placeholder="Sender"
-        className="w-full p-2 border rounded-md mb-2"
-        value={newMessage.sender}
-        onChange={handleMessageChange}
-      />
-      <input
-        type="text"
-        name="subject"
-        placeholder="Subject"
-        className="w-full p-2 border rounded-md mb-2"
-        value={newMessage.subject}
-        onChange={handleMessageChange}
-      />
-      <textarea
-        name="body"
-        placeholder="Message Body"
-        className="w-full p-2 border rounded-md mb-4"
-        value={newMessage.body}
-        onChange={handleMessageChange}
-      />
-      <div className="flex justify-end">
-        <button
-          onClick={handleSendMessage}
-          className="bg-custom-blue text-white px-4 py-2 rounded-md hover:bg-blue-600"
-        >
-          Send
-        </button>
-        <button
-          onClick={() => setIsComposing(false)}
-          className="ml-4 text-gray-500"
-        >
-          Cancel
-        </button>
-      </div>
-    </div>
-  </div>
-)}
+        {isComposing && (
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
+            <div className="p-6 bg-white shadow-md rounded-md w-1/3">
+              <div className="flex justify-between">
+                <h3 className="text-xl font-semibold mb-4">Compose Message</h3>
+                <button
+                  onClick={() => setIsComposing(false)}
+                  className="text-gray-500 mb-4"
+                >
+                  <FaTimes className="text-custom-blue " />
+                </button>
+              </div>
+              <input
+                type="text"
+                name="sender"
+                placeholder="Sender"
+                className="w-full p-2 border rounded-md mb-2"
+                value={newMessage.sender}
+                onChange={handleMessageChange}
+              />
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                className="w-full p-2 border rounded-md mb-2"
+                value={newMessage.subject}
+                onChange={handleMessageChange}
+              />
+              <textarea
+                name="body"
+                placeholder="Message Body"
+                className="w-full p-2 border rounded-md mb-4"
+                value={newMessage.body}
+                onChange={handleMessageChange}
+              />
+              <div className="flex justify-end">
+                <button
+                  onClick={handleSendMessage}
+                  className="bg-custom-blue text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                >
+                  Send
+                </button>
+                <button
+                  onClick={() => setIsComposing(false)}
+                  className="ml-4 text-gray-500"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
 
         {selectedMessage ? (
           <div className="p-6 bg-white shadow-md rounded-md mb-4">
@@ -269,7 +274,9 @@ const MailboxComponent = () => {
             >
               <FaArrowLeft className="mr-2 text-custom-blue" /> Back
             </button>
-            <h3 className="text-xl font-semibold mb-4">{selectedMessage.subject}</h3>
+            <h3 className="text-xl font-semibold mb-4">
+              {selectedMessage.subject}
+            </h3>
             <p className="text-gray-500 mb-4">{selectedMessage.sender}</p>
             <p>{selectedMessage.fullMessage}</p>
           </div>
@@ -292,7 +299,7 @@ const MailboxComponent = () => {
             )}
           </div>
         )}
-      </div>
+      {/* </div> */}
     </div>
   );
 };
