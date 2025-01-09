a94bfba0363a2476df095aef56de5cf3234f4be1
Raghav Kukkar <kukkarraghav02@gmail.com>
2025-01-10 00:27:12 +0530

    Home Graph Build

--- backend/routes/placement.js
+++ backend/routes/placement.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllPlacements, getFilteredPlacements, getLastSevenDaysPlacements, getTodayPlacements } from "../controller/placement.js";
+import { getAllPlacements, getCombinedInsights, getFilteredPlacements, getLastSevenDaysPlacements, getPlacementInsights, getTodayPlacements } from "../controller/placement.js";
 const router=express.Router();
 
 router.get("/today",getTodayPlacements);
 router.get("/last-seven-days",getLastSevenDaysPlacements);
 router.get("/",getAllPlacements);
 router.get("/filter",getFilteredPlacements);
+router.get("/insights",getPlacementInsights);
+router.get("/cominsights",getCombinedInsights);
 
 export default router;
