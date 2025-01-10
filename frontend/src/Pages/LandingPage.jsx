3ae31373ab568886752f546d2541a6e8c4590e05
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2025-01-10 22:09:47 +0530

    Update LandingPage.jsx

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -21,7 +21,6 @@ const Home = () => {
       <div className=" max-w-7xl mx-auto "><AboutUs /></div>
       <div className="max-w-7xl mx-auto "><TpoMessage /></div>
       <div className="max-w-7xl mx-auto mt-24"><RecruitmentProcess /></div>
-      <div className=" mt-24 pt-15 "><WhyRecruit /></div>
       <div className="max-w-7xl mx-auto  "><WhyRecruit /></div>
       <div className="max-w-7xl mx-auto "><PlacementHighlights /></div> 
       <div className="max-w-7xl mx-auto mt-24 mb-40 pt-15 px-6"><ImageSlider/></div>   
