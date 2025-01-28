3f504cd8f39c77de98cad4bec210f6836c3d0d42
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2025-01-28 22:10:06 +0530

    improved login

---
+++ backend/routes/companiesanalysis.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+/* import { getCompanyAnalytics } from "../controllers/analyticsController.js";
+
+router.get("/get", getCompanyAnalytics); */
+
+export default router;
