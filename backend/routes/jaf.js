7f8b0995bdccee68fd5d11b22976f922e77d351d
Raghav Kukkar <kukkarraghav02@gmail.com>
2025-02-01 11:55:56 +0530

    JAF

--- backend/routes/jaf.js
+++ backend/routes/jaf.js
@@ -2,10 +2,16 @@ import express from "express";
 const router=express.Router();
 
 import {
-    createJobAnnouncementForm,getjaf
+    approveJAF,
+    createJobAnnouncementForm,getjaf,
+    rejectJAF,
+    updateJAF
     } from "../controller/jaf.js";
 
 router.post("/create", createJobAnnouncementForm);
 router.get("/get",getjaf);
+router.put("/approvejaf/:_id", approveJAF);
+router.put("/rejectjaf/:_id", rejectJAF);
+router.put("/updatejaf/:_id", updateJAF);
 
 export default router;
